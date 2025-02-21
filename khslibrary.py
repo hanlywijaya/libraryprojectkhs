ff4585d9bea273e44e399b0d0a6fd3e668f1e56d
hanlywijaya <57889649+hanlywijaya@users.noreply.github.com>
2025-02-21 23:13:23 +1100

    Get to the basics of database and setting up login. working locally for now and update soon.

---
+++ khslibrary.py
@@ -0,0 +1,47 @@
+## USER DATABASE
+users = {
+    'sgilmore': 'Pass1',
+    'bwiseman': 'Pass2',
+    'uhalaj': 'Pass3',
+    'gwang': 'Pass4',
+    'mkassar': 'Pass5',
+    'adib': 'Pass6',
+    'bwiseman2': 'Pass7',
+    'kjohnson': 'Pass8',
+    'fjose': 'Pass9',
+    'mmansour': 'Pass10',
+}
+
+fullnames = {
+    'sgilmore': 'Sam Gilmore', 
+    'bwiseman': 'Bella Wiseman',
+    'uhalaj': 'Umar Halaj',
+    'gwang': 'Qing Wang',
+    'mkassar': 'Maryam Kassar',
+    'adib': 'Ali Dib',
+    'bwiseman2': 'Benji Wiseman',
+    'kjohnson': 'Khadija Johnson',
+    'fjose': 'Fiona Jose',
+    'mmansour': 'Mathew Mansour',
+}
+
+print("Welcome to Kingsgrove High School's library services.")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+
+## login
+def login():
+    attempts=3
+    while attempts > 0:
+        username = input("Please enter your username: ")
+        password = input("Please enter your password: ")
